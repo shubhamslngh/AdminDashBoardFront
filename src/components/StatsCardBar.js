ada844c5e91af4622b624bfb24fce4595fa036cb
shubham singh <shubhamshivam.singh@gmail.com>
2024-07-02 14:57:11 +0530

    updates to dashboard

--- src/components/StatsCardBar.js
+++ src/components/StatsCardBar.js
@@ -5,6 +5,7 @@ import WorldChart from "./WorldChart"; // Import the WorldChart component
 const StatsCardBar = ({ chartData }) => {
     return (
         <div className="stats-card">
+            <h1>Official Data</h1>
             <div className="chart-container h-[100px] w-[100px]">
                 <WorldChart data={chartData} />
             </div>
